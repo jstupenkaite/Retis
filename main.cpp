59375df2290312883bb0db7f9491dfcd259e1dd8
Remigijus P <remziukas@gmail.com>
2012-11-06 17:34:04 +0200

    Naudoja dinaminius masyvus (vector)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 static const int N = 1000;
 int main(){
-	int i, a[N];
+	int i;
+	vector <int> a(N);
+	cout << "masyvo dydis: " << a.size() << "\n";
+	cout << "masyvo talpa: " << a.capacity() << "\n";
+	cout << "maksimali talpa: " << a.max_size() << "\n";
     for (i = 2; i < N; i++) a[i] = 1;
     for (i = 2; i < N; i++)
       if (a[i])
@@ -10,4 +15,9 @@ int main(){
     for (i = 2; i < N; i++)
       if (a[i]) cout << " " << i;
     cout << endl;
+	for (int i=1; i<10; i++)
+		a.push_back(i);
+	cout << "masyvo dydis: " << a.size() << "\n";
+	cout << "masyvo talpa: " << a.capacity() << "\n";
+	cout << "maksimali talpa: " << a.max_size() << "\n";
 }
